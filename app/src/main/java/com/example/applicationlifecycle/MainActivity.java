f5a1162b686f08fc12f229a6e5c17382ce854a26
Uzair390-Del <uzair.zairi321@gmail.com>
2025-03-24 14:22:17 +0500

    updates

--- app/src/main/java/com/example/applicationlifecycle/MainActivity.java
+++ app/src/main/java/com/example/applicationlifecycle/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.applicationlifecycle;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +14,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    TextView textView;
+    Button btn1;
+    Button btn2;
+    int counter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +27,24 @@ public class MainActivity extends AppCompatActivity {
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-
+            textView=findViewById(R.id.textView);
+            btn1=findViewById(R.id.btn1);
+            btn2=findViewById(R.id.btn2);
+            btn1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                 counter=counter+1;
+                 textView.setText(""+counter);
+                }
+            });
+            btn2.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent i = new Intent(getApplicationContext(), MainActivity2.class);
+                    startActivity(i);
+                }
+            });
+            Log.d("Message","First Activity onCreate");
             return insets;
         });
 
@@ -28,31 +53,31 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        Log.d("Main Activity","The application onStart message is called");
+        Log.d("Main Activity","First Activity onStart message is called");
 
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.d("Main Activity","The application onResume message is called");
+        Log.d("Main Activity","First Activity onResume message is called");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Log.d("Main Activity","The application onPause message is called");
+        Log.d("Main Activity","First Activity onPause message is called");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Log.d("Main Activity","The application onStop message is called");
+        Log.d("Main Activity","First Activity onStop message is called");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.d("Main Activity","The application destroy message is called");
+        Log.d("Main Activity","First Activity destroy message is called");
     }
 }
--- app/src/main/java/com/example/applicationlifecycle/MainActivity2.java
+++ app/src/main/java/com/example/applicationlifecycle/MainActivity2.java
@@ -1,6 +1,10 @@
 package com.example.applicationlifecycle;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +14,58 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity2 extends AppCompatActivity {
 
+    Button btn3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main2);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main2), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+
+            btn3=findViewById(R.id.btn3);
+            btn3.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(i);
+                }
+            });
+            Log.d("Message","Second Activity onCreate");
             return insets;
+
         });
     }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("Main Activity","Second Activity onStart message is called");
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("Main Activity","Second Activity onResume message is called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("Main Activity","Second Activity onPause message is called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("Main Activity","Second Activity onStop message is called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("Main Activity","Second Activity destroy message is called");
+    }
+
 }
